6384cfb12dbd2cc54773241ca86ea4c3583df324
Abdelmouhaymine Salah <asalah@e2r9p13.1337.ma>
2021-05-11 11:23:56 +0000

    fix reviews

--- components/HomePage/Reviews.vue
+++ components/HomePage/Reviews.vue
@@ -14,7 +14,7 @@
               <v-carousel-item v-for="avatar in avatars" :key="avatar.name">
                 <v-col>
                   <v-row align="center" justify="center" class="my-6 mx-10">
-                    <h1 align="center" justify="center">{{ avatar.text }}</h1>
+                    <h1 class="black--text" color="primary" align="center" justify="center">{{ avatar.text }}</h1>
                   </v-row>
                   <v-row align="center" justify="center" class="mx-10">
                     <v-avatar size="80" class="pink darken-3 mt-6" align="center" justify="center">
@@ -22,7 +22,7 @@
                     </v-avatar>
                   </v-row>
                   <v-row align="center" justify="center">
-                    <h4 class="mt-2" align="center" justify="center">{{ avatar.name }}</h4>
+                    <h3 class="mt-2 black--text" align="center" justify="center">{{ avatar.name }}</h3>
                   </v-row>
                 </v-col>
               </v-carousel-item>
@@ -41,35 +41,35 @@ export default {
       avatars: [
         {
           name: "Elliop",
-          text: "JobZilla thanks for giving cvbvcbcvvbx wesome hobby",
+          text: "JobZilla thanks for giving us this wesome hobby",
           pic: "/avatar-1.png"
         },
         {
-          name: "estarossa",
-          text: "JobZillzxvds dfg df srth gfh new awesome hobby",
+          name: "Estarossa",
+          text: "JobZilla Best way ever of learning how to solve problems",
           pic: "/avatar-2.png"
         },
         {
           name: "SpiderX",
-          text: "I love logic, math, comdfgh fgh dfgprogramming.",
+          text: "I Found this website today! It's juste amazing!",
           pic: "/avatar-3.png" 
         },
         {
-          name: "agourrag",
+          name: "Agourrag",
           text:
-            " I don't know either JobZilla thanks fw awesome hobby",
+            "All I can say is that JobZilla is awesome!",
           pic: "/avatar-4.png"
         },
         {
-          name: "droven",
+          name: "Droven",
           text:
-            "JobZilla droven  thanks droven odroven r dfgh  awesome hobby",
+            "JobZilla looks like a great way to learn",
           pic: "/avatar-5.png"
         },
         {
           name: "Joker",
           text:
-            " joker df df gfd fgdh fgx JobZilla t gfd hew awesome hobby",
+            "JpbZilla's contest was the most fun I've had programming in a while",
           pic: "/avatar-6.png"
         }
       ]
